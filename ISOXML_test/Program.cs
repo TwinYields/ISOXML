5261c7ee02c39ae9ad50db4bab1ec72845514be3
Matti Pastell <matti.pastell@helsinki.fi>
2023-05-23 16:21:19 +0300

    Add console

---
+++ ISOXML_test/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ISOXML;
+
+var tldata = ISOXML.TimeLogReader.ReadTaskFile("TASKDATA_20210603_0159\\TASKDATA.XML");
+
+Console.WriteLine("Hello, World!");
+
